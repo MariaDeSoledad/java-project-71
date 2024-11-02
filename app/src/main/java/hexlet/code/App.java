95ade025d497deb514f4da410dc43581cd19b0da
имя фамилия < >
2024-11-02 16:47:15 +0300

    final update

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,12 +5,17 @@ import picocli.CommandLine.Option;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
 
+import java.util.concurrent.Callable;
+
+import static hexlet.code.Difffer.generate;
+
 
 @Command(
         name = "gendiff",
         description = "Compares two configuration files and shows a difference."
 )
-public class App implements Runnable {
+public class App implements Callable<Integer> {
+    private static final int SUCCESS_EXIT_CODE = 0;
     @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
     private String filePath1;
     @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
@@ -22,13 +27,16 @@ public class App implements Runnable {
     private boolean help;
     @Option(names = { "-V", "--version" }, versionHelp = true, description = "Print version information and exit.")
     private boolean version;
-    public static void main(String[] args) {
-        int exitCode = new CommandLine(new App()).execute(args);
-        System.exit(exitCode);
-    }
 
     @Override
-    public void call() throws Exception{
+    public Integer call() throws Exception {
+        String diff = generate(filePath1, filePath2, format);
+        System.out.println(diff);
+        return SUCCESS_EXIT_CODE;
+    }
 
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
